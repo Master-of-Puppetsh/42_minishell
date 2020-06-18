bb2849ec0ec52bc68084adb2ff793dba7afbe333
DiceMono <qrdfg@naver.com>
2020-06-18 19:13:22 +0900

    finish

--- src/buitin/ft_exit.c
+++ src/buitin/ft_exit.c
@@ -6,17 +6,18 @@
 /*   By: hyekim <hyekim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 15:16:43 by hyekim            #+#    #+#             */
-/*   Updated: 2020/06/17 16:08:27 by hyekim           ###   ########.fr       */
+/*   Updated: 2020/06/18 17:59:36 by hyekim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #	include "minishell.h"
 
-int		ft_exit(char **argv)
+int		ft_exit(char **argv, int status)
 {
 	if (argv[1] && argv[2])
 		return (ERROR); // TOO MANY ARGUMENT
-	if (argv[])
-		exit()
-	return (SUCCESS);
+	ft_putendl_fd("exit", STDOUT_FILENO);
+	if (!argv[1])
+		exit(status);
+	exit(ft_atoi(argv[1]));
 }
--- src/buitin/ft_pwd.c
+++ src/buitin/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: hyekim <hyekim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 02:18:24 by hyekim            #+#    #+#             */
-/*   Updated: 2020/06/17 14:58:45 by hyekim           ###   ########.fr       */
+/*   Updated: 2020/06/18 18:00:05 by hyekim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	ft_pwd(char **argv)
 {
-	char	**argv;
 	char	path[1024];
 
 	if (getcwd(path, 1024) == NULL)
-		return ; // getcwd err
-	ft_putstr_fd(path, 1);
+		return ;
+	ft_putendl_fd(path, 1);
+	return ;
 }
